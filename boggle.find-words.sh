3da74836f4a586c5d6627220c5d8e08284afbc83
Cory Washington <tbd>
2021-12-15 22:49:32 -0600

    VERY early version of 'find words' script
    Right now it just declares some files, chooses a grid and word list file
    to use, and checks that the selected files exist and are readable.

---
+++ boggle.find-words.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# File on errors. Undefined vars count as errors.
+set -eu
+
+# Declare some files for later selection
+GRID_01="/home/cory/git/boggle/data/grids/sample-boggle-grid.01.txt2"
+GRID_03="/home/cory/git/boggle/data/grids/sample-boggle-grid.03.txt"
+GRID_02="/home/cory/git/boggle/data/grids/sample-boggle-grid.02.txt"
+
+AMERICAN_ENGLISH_WORDS="/home/cory/git/boggle/data/words/american-english.boggle.words"
+BOGGLE_WORDS="/home/cory/git/boggle/data/words/international-english.boggle.words"
+
+BOGGLE_DICE_TXT="/home/cory/git/boggle/data/dice/boggle-dice.txt"
+
+# Exit codes (to be OR'd together ('|'))
+declare -i FILE_MISSING=1
+declare -i FILE_UNREADABLE=2
+
+# Select specific files to use this run
+GRID="${GRID_01}"
+WORDS="${AMERICAN_ENGLISH_WORDS}"
+
+# Verify chosen files exist
+declare -i exitCode=0
+if [ ! -f "${GRID}" ]; then
+  echo "ERROR: Boggle grid file '${GRID}' does not exist"
+  exitCode=$((exitCode | FILE_MISSING))
+elif [ ! -r "${GRID}" ]; then
+  echo "ERROR: Boggle grid file '${GRID}' exists but it cannot be read"
+  exitCode=$((exitCode | FILE_UNREADABLE))
+fi
+
+if [ ! -f "${WORDS}" ]; then
+  echo "ERROR: Boggle words file '${WORDS}' does not exist"
+  exitCode=$((exitCode | FILE_MISSING))
+elif [ ! -r "${WORDS}" ]; then
+  echo "ERROR: Boggle words file '${WORDS}' exists but it cannot be read"
+  exitCode=$((exitCode | FILE_UNREADABLE))
+fi
+
+if [ ${exitCode} -gt 0 ]; then
+  echo
+  echo "An error was encountered. Exiting early with exit code ${exitCode}"
+  exit ${exitCode}
+else
+  echo "PASS !!!!"
+fi
