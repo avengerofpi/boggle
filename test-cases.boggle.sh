afe20b9f52b7d569bbae0961e479460367eb4330
Cory Washington <tbd>
2022-01-15 19:59:01 -0500

    Improve testing script (significantly)
    All test cases run in parallel now, using nohup.
    Results are summarized, waiting for all tests to complete.
    Better filename/path structure, including setting up a dedicated test
      results output dir, just like what the boggle script itself does.
    Better logging, but not yet using custom log functions
    Use shorthand 'labels' for grid and words files and functions to expand
      the labels into full pathnames, to succinctly declare and conveniently
      iterate over grid and words files.
    
    Semantic changes:
    Tweak functions from using positional params to using existing vars.
    Re-enable 'set -eu' bash shell variables except turn off '-e' when
      calling boggle script itself.

--- test-cases.boggle.sh
+++ test-cases.boggle.sh
@@ -1,47 +1,136 @@
 #!/bin/bash
 
 # Exit on errors. Undefined vars count as errors.
-#set -eu
+set -eu
 
-# Grids files
-g01="${PWD}/data/grids/sample-boggle-grid.01.txt"
-g02="${PWD}/data/grids/sample-boggle-grid.02.txt"
-g03="${PWD}/data/grids/sample-boggle-grid.03.txt"
-g04="${PWD}/data/grids/sample-boggle-grid.04.txt"
-g05="${PWD}/data/grids/sample-boggle-grid.05.txt"
-g06="${PWD}/data/grids/sample-boggle-grid.06.txt"
-g07="${PWD}/data/grids/sample-boggle-grid.07.txt"
-g08="${PWD}/data/grids/sample-boggle-grid.08.txt"
-g09="${PWD}/data/grids/sample-boggle-grid.09.txt"
+# Select an output dir to use
+declare -i outputDirIndex=""
+declare outputDir=""
+function setupOutputDir() {
+  baseOutputDir="${PWD}/data/test/results"
+  outputDirFormatStr="${baseOutputDir}/%05d"
+  outputDir=""
+  for outputDirIndex in {0..99999}; do
+    putative_outputDir="$(printf "${outputDirFormatStr}" "${outputDirIndex}")"
+    if [ ! -e "${putative_outputDir}" ]; then
+      outputDir="${putative_outputDir}"
+      break;
+    fi;
+  done;
 
-# Words files
-wAme="${PWD}/data/words/american-english.boggle.words"
-wInt="${PWD}/data/words/international-english.boggle.words"
-
-# TODO: keep track of pass and fail combinations to summarize at the end
-# TODO: maybe set an error code if 1 or more tests fail
-# TODO: maybe use colored logging here, too? try to not compete with color scheme for boggle script
-#       maybe make coloration a sourcable file, for both/all scripts
-function runTestCase() {
-  declare -i numArgs=${#@}
-  if [ ${numArgs} -ne 3 ]; then
-    echo "ERROR: function '${FUNCNAME[0]}' expectes three inputs value but received ${numArgs}"
+  if [ -z "${outputDir}" ]; then
+    logError "Failed to choose a usable output dir."
     exit 1
   fi
 
+  mkdir -p "${outputDir}"
+  echo "Output dir: ${outputDir}"
+}
+
+# Grids files
+GRID_FILE_FORMAT_STR="${PWD}/data/grids/sample-boggle-grid.%s.txt"
+GRID_FILE_LABELS=({01..09})
+# Depends on: gridFileLabel
+function setGridFile() {
+  gridFile="$(printf "${GRID_FILE_FORMAT_STR}" "${gridFileLabel}")"
+}
+
+# Words files
+WORDS_FILE_FORMAT_STR="${PWD}/data/words/%s.boggle.words"
+WORDS_FILE_LABELS=(
+  "american-english"
+  "international-english"
+)
+# Depends on: wordFileLabel
+function setWordFile() {
+  wordFile="$(printf "${WORDS_FILE_FORMAT_STR}" "${wordFileLabel}")"
+}
+
+# Expect filtered word lists
+EXPECTED_WORDS_FILE_FORMAT_STR="${PWD}/data/test/expected-words/expected-words.%s.grid-%s.txt"
+# Depends on: wordFileLabel gridFileLabel
+function setTestFile() {
+  testFile="$(printf "${EXPECTED_WORDS_FILE_FORMAT_STR}" "${wordFileLabel}" "${gridFileLabel}")"
+}
+
+# TODO: maybe use colored logging here, too? try to not compete with color scheme for boggle script
+# Depends on: wordFileLabel gridFileLabel
+function runTestCase() {
+  setGridFile
+  setWordFile
+  setTestFile
+
+  outputFile="${outputDir}/${wordFileLabel}.${gridFileLabel}.txt"
+  outputFiles+=("${outputFile}")
   echo "Running TestCase with"
-  echo "  Grid file: ${1}"
-  echo "  Word file: ${2}"
-  echo "  Expected filtered file: ${3}"
-  time ./boggle.find-words.sh -g "${1}" -w "${2}" --no-info --no-debug --test "${3}"
+  echo "    Grid file: ${gridFile}"
+  echo "    Word file: ${wordFile}"
+  echo "    Test file: ${testFile}"
+  echo "  Output file: ${outputFile}"
+
+  colorFlag="--no-color"
+  debugFlag="--no-debug"
+  infoFlag="--info"
+  warnFlag="--warn"
+  errorFlag="--error"
+  scoreFlag="--score"
+  loggingFlags="${colorFlag} ${debugFlag} ${infoFlag} ${warnFlag} ${errorFlag} ${scoreFlag}"
+
+  set +e
+  nohup /usr/bin/time -pao "${outputFile}" ./boggle.find-words.sh -g "${gridFile}" -w "${wordFile}" -t "${testFile}" ${loggingFlags} > "${outputFile}" &
+  set -eu
   echo
 }
 
-for wType in "american" "international"; do
-  for gi in {01..09}; do
-    g="${PWD}/data/grids/sample-boggle-grid.${gi}.txt"
-    w="${PWD}/data/words/${wType}-english.boggle.words"
-    t="${PWD}/data/test/words.sample-boggle-grid.${gi}.txt---${wType}-english.boggle.words---filtered.txt"
-    runTestCase "${g}" "${w}" "${t}"
+# Check output files and summarize results
+outputFiles=()
+summarizeResults() {
+  failPattern="Test FAILURE"
+  passPattern="Test SUCCESS"
+  declare -i numFiles=$(ls -1                   "${outputFiles[@]}" | wc -l)
+  declare -i numPass=$(grep -l "${passPattern}" "${outputFiles[@]}" | wc -l)
+  declare -i numFail=$(grep -l "${failPattern}" "${outputFiles[@]}" | wc -l)
+
+  declare -i numGridFiles=${#GRID_FILE_LABELS[@]}
+  declare -i numWordFiles=${#WORDS_FILE_LABELS[@]}
+  declare -i expectedNumFiles=${#outputFiles[@]}
+
+  # Make sure we have the expected number of outpuf files
+  if [ ${numFiles} -ne ${expectedNumFiles} ]; then
+    echo "Error: there are ${numFiles} output files but we expected to find ${expectedNumFiles}"
+    exit 2
+  fi
+
+  echo "Waiting for all tests to pass..."
+  declare -i totalDone=$((numPass + numFail))
+  declare -i sleepSeconds=10
+  while [ ${numFiles} -ne ${totalDone} ]; do
+    printf "  Not done yet - %2d of %2d tests finished - sleeping another %d seconds\n" ${totalDone} ${expectedNumFiles} ${sleepSeconds}
+    sleep ${sleepSeconds}
+    numPass=$(grep -l "${passPattern}" "${outputFiles[@]}" | wc -l)
+    numFail=$(grep -l "${failPattern}" "${outputFiles[@]}" | wc -l)
+    totalDone=$((numPass + numFail))
   done
-done
+  echo
+  printf "%2d test report files\n"  ${numFiles}
+  printf "%2d tests passed\n"       ${numPass}
+  printf "%2d tests failed\n"       ${numFail}
+}
+
+declare wordFileLabel gridFileLabel
+declare -a outputFiles=()
+function runTestCases() {
+  for wordFileLabel in "${WORDS_FILE_LABELS[@]}"; do
+    for gridFileLabel in "${GRID_FILE_LABELS[@]}"; do
+      runTestCase "${gridFileLabel}" "${wordFileLabel}"
+    done
+  done
+}
+
+main() {
+  setupOutputDir
+  runTestCases
+  summarizeResults
+}
+
+main
